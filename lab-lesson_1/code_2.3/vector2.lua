8f618e43646c1155057b5d4642de138fb6b9ebf2
Edoardo Barbieri <edoardo.barbieri.96@gmail.com>
2020-03-22 14:41:15 +0100

    Update vector2.lua

--- lab-lesson_1/code_2.3/vector2.lua
+++ lab-lesson_1/code_2.3/vector2.lua
@@ -1,10 +1,9 @@
 v2_mt = {
   __add = function(left,right) return v2(left.x + right.x, left.y + right.y) end,
-	__mul = function(left,right) return v2(left.x * right, left.y * right) end,
-	__sub = function(left, right) return v2(left.x - right.x, left.y - right.y) end,
-	__unm = function(left) return v2(-left.x , -left.y) end
+  __mul = function(left,right) return v2(left.x * right, left.y * right) end,
+  __sub = function(left, right) return v2(left.x - right.x, left.y - right.y) end,
+  __unm = function(left) return v2(-left.x , -left.y) end
 }
-
 
 function v2(X, Y)
 	v = { 
@@ -13,7 +12,7 @@ function v2(X, Y)
     length = function() return math.sqrt(X ^ 2 + Y ^ 2) end,
     tostring = function() return "[" .. X ..", " .. Y .. "]" end,
     type = "v2"
-	}
-	setmetatable(v, v2_mt);
-	return v
+  }
+  setmetatable(v, v2_mt);
+  return v
 end
